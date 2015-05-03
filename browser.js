18eb3466a385125669004571fe248d2fa2ad1745
Mikhail Novikov <sigod.mail@gmail.com>
2015-05-03 20:14:03 +0300

    refactor

--- browser.js
+++ browser.js
@@ -28,7 +28,7 @@
 			main = getId(__path, main);
 
 			loadModule(main, true, function () {
-				getModule(cache[main]);
+				getExports(cache[main]);
 			});
 		}
 	}
@@ -36,7 +36,7 @@
 	// ---------------------------------------------------------------------------------------------------------------------
 
 	function browserjs(func) {
-		processModule(__path, func, function () {
+		processModule(__path, func.toString(), function () {
 			invokeModule(__path, func);
 		});
 	}
@@ -52,10 +52,10 @@
 			cached = cache[id];
 		}
 
-		return getModule(cached);
+		return getExports(cached);
 	}
 
-	function getModule(module) {
+	function getExports(module) {
 		if (module.error) throw new Error("Cannot find module '" + module.id + "'");
 
 		if (module.func) {
@@ -85,23 +85,27 @@
 		return pmodule.exports;
 	}
 
-	function processModule(path, func, callback) {
+	function processModule(path, content, callback) {
 		var modules = [];
 
-		parseRequires(func.toString())
+		parseRequires(content)
 			.forEach(function (module) {
 				var resolved = getId(path, module);
 
 				if (!cache[resolved]) modules.push(resolved);
 			});
 
-		if (!modules.length) return callback();
+		if (modules.length) {
+			var count = modules.length;
+			function fn() {
+				if (--count === 0) callback();
+			}
 
-		var ncall = new NCall(modules.length, callback);
-
-		modules.forEach(function (module) {
-			loadModule(module, true, ncall.callback);
-		});
+			modules.forEach(function (module) {
+				loadModule(module, true, fn);
+			});
+		}
+		else callback();
 	}
 
 	var require_begin_str = 'require(';
@@ -157,26 +161,23 @@
 	// ---------------------------------------------------------------------------------------------------------------------
 
 	function getId(parent, module) {
-		var id = join(removeFilename(parent), module);
-		var ext = getExtension(id);
+		var id = buildPath(folder(parent), module);
 
-		if (!ext) id += '.js';
+		if (!extension(id)) id += '.js';
 
 		return id;
 	}
 
-	function removeFilename(path) {
+	function folder(path) {
 		return path.substr(0, path.lastIndexOf('/') + 1);
 	}
 
-	function getExtension(path) {
+	function extension(path) {
 		var index = path.lastIndexOf('.');
-		if (index === -1) return null;
-
-		return path.substring(index);
+		if (index !== -1) return path.substring(index);
 	}
 
-	function join() {
+	function buildPath() {
 		return normalize(Array.prototype.join.call(arguments, '/'));
 	}
 
@@ -196,21 +197,6 @@
 
 		return path.join('/');
 	}
-
-	function NCall(number, callback) {
-		this._number = number;
-		this._number_called = 0;
-		this._callback = callback;
-		this.callback = this.callback.bind(this);
-	}
-
-	NCall.prototype.callback = function () {
-		++this._number_called;
-
-		if (this._number === this._number_called) {
-			this._callback();
-		}
-	};
 
 	// ---------------------------------------------------------------------------------------------------------------------
 })();
